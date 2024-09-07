5198fd7131175009210ffe69c74a00c113b1af24
mirza-syahmi-tn <syahmi.jamal@tiongnam.com.my>
2024-09-07 23:59:34 +0800

    Add TimeLine component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import TimeLine from '@/components/TimeLine.vue'
 import { SPACE_X_BASE_URL } from '@/constants'
 import type { Launch } from '@/types'
 import { useFetch } from '@vueuse/core'
@@ -7,13 +8,16 @@ const { data, error, isFetching } = useFetch(`${SPACE_X_BASE_URL}/launches`).get
 </script>
 
 <template>
-  <main>
-    <div v-if="error">{{ error }}</div>
-    <div v-else-if="isFetching">Loading...</div>
-    <div v-else>
-      <pre v-for="launch in data" :key="launch.flight_number">
-        {{ new Date(launch.launch_date_unix) }}
-      </pre>
+  <main class="flex w-full">
+    <div class="w-1/3 m-auto text-center">
+      <h1 class="text-white font-bold text-5xl">Launches</h1>
+    </div>
+    <div class="w-2/3">
+      <div v-if="error">{{ error }}</div>
+      <div v-else-if="isFetching">Loading...</div>
+      <div v-else class="h-[100dvh] overflow-y-auto">
+        <TimeLine :launches="data" />
+      </div>
     </div>
   </main>
 </template>
